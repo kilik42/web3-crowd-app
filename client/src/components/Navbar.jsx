35cc2f12f6a2c8a1f11d6eff493d00ed6acd3609
kilik42 <marvinevins@yahoo.com>
2023-02-26 22:44:42 -0600

    working on web3crowd

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import {HiMenuAlt4} from 'react-icons/hi';
+import {AiOutlineClose} from 'react-icons/ai';
 
+import logo from '../../assets/logo.png'
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='w-full flex md:justify-between justify-center items-center py-4 px-4 md:px-8'>
+      <div className='flex items-center'>
+        
+        </div>
+      </nav>
   )
 }
 
